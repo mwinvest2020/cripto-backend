04e2d89a3d053ed5f473c89a6e32521acb6cc3c5
mwinvest2020 <mwinvest2020@gmail.com>
2024-09-16 02:00:32 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const { createClient } = require("@supabase/supabase-js");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Configurando o Supabase
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+// Rota de teste
